e88b5d0bcaf334a202cf5dc4ef832314cb840ff7
dkdkxkkc <1146636001@qq.com>
2026-04-17 08:58:48 +0800

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 120000,
+  retries: 0,
+  workers: 1,
+  reporter: [['list']],
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'off'
+  }
+});
